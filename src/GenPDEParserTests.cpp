81e2e89484773ad3ba8dcb7c5f94fba5cba32251
196884 <regis.dupont+git@m4x.org>
2015-04-03 09:13:22 -0400

    Adding test for model parsing

--- src/GenPDEParserTests.cpp
+++ src/GenPDEParserTests.cpp
@@ -1,11 +1,3 @@
-//
-//  GenPDEParserTests.cpp
-//  GenPDE
-//
-//  Created by Regis Dupont on 8/31/14.
-//  Copyright (c) 2014 Regis Dupont. All rights reserved.
-//
-
 #include <iostream>
 #include <fstream>
 
---
+++ src/ModelParserTests.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+#include "Framework.h"
+#include "TestSuite.h"
+
+#include "ModelParser.h"
+
+REGISTER_TEST(BSModelParsing1)
+{
+    std::string file("../resources/PDE_BS_Model_1.xml");
+    boost::shared_ptr<PDEPricingModelInterface> pe(modelParser::parsePDEModel(file, true));
+    TEST(true);
+}
+
+
